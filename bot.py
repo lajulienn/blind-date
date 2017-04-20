dad3676cfa4be8ed1ac34c43e204b5a24cb94767
Julia Ivanova <ivanova-j-ulia@yandex.ru>
2017-04-20 12:38:35 +0300

    Improve start handler

--- bot.py
+++ bot.py
@@ -4,56 +4,64 @@ import telebot
 
 bot = telebot.TeleBot(config.token)
 
-# opened_dialogues = shelve.open(config.opened_dialogues_db)
-# closed_dialogues = shelve.open(config.closed_dialogues_db)
 opened_dialogues = {}
 closed_dialogues = {}
 waiting_queue = []
 
-# with shelve.open(config.users_db) as users:
+with shelve.open(config.users_db) as users:
+
+    def is_opened(user_id):
+        return user_id in opened_dialogues \
+               or (len(waiting_queue) != 0 and user_id == waiting_queue[0])
 
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, 'Finding a partner for you...')
-    if message.chat.id in opened_dialogues or (len(waiting_queue) != 0 and id == waiting_queue[0]):
-        return
+    def is_smb_available(user_id):
+        return len(waiting_queue) != 0 and (user_id not in closed_dialogues
+                                            or closed_dialogues.get(user_id) != waiting_queue[0])
 
-    # user = message.from_user.username
-    # if user not in users:
-    #     users[message.chat.id] = (user, False)  # id -> (username, revealed)
 
-    def is_smb_available(id):
-        return len(waiting_queue) != 0 and (id not in closed_dialogues
-                                           or closed_dialogues.get(id) != waiting_queue[0])
-
-    if not is_smb_available(message.chat.id):
-        waiting_queue.append(message.chat.id)
-    else:
-        first = message.chat.id
-        second = waiting_queue.pop(0)
-        opened_dialogues[first] = second
-        opened_dialogues[second] = first
+    def remove_from_closed(first, second):
         if first in closed_dialogues:
             del closed_dialogues[first]
         if second in closed_dialogues:
             del closed_dialogues[second]
-        bot.send_message(first, 'Start talking!')
-        bot.send_message(second, 'Start talking!')
 
 
-@bot.message_handler(commands=['help'])
-def send_help(message):
-    bot.send_message(message.chat.id, 'Help')
+    def add_new_user(user_id, username):
+        users[str(user_id)] = config.UserProperties(username, False)
+
+    @bot.message_handler(commands=['start'])
+    def start(message):
+        user_id = message.chat.id
+        bot.send_message(user_id, 'Finding a partner for you...')
+
+        if is_opened(user_id):
+            return
+
+        username = message.from_user.username
+        add_new_user(user_id, username)
+
+        if not is_smb_available(user_id):
+            waiting_queue.append(user_id)
+        else:
+            first = user_id
+            second = waiting_queue.pop(0)
+            opened_dialogues[first] = second
+            opened_dialogues[second] = first
+            remove_from_closed(first, second)
+            bot.send_message(first, 'Start talking!')
+            bot.send_message(second, 'Start talking!')
 
 
-@bot.message_handler(content_types=['text'])
-def reply(message):
-    bot.send_message(opened_dialogues[message.chat.id], message.text)
+    @bot.message_handler(commands=['help'])
+    def send_help(message):
+        bot.send_message(message.chat.id, 'Help')
 
 
-# receiving new messages
-bot.polling()
+    @bot.message_handler(lambda: True)
+    def reply(message):
+        bot.send_message(opened_dialogues[message.chat.id], message.text)
 
-# opened_dialogues.close()
-# closed_dialogues.close()
+
+    bot.polling()
+
--- config.py
+++ config.py
@@ -1,4 +1,8 @@
+from collections import namedtuple
+
 token = '343052840:AAHQHcniRnEPMWWL23oLuFC6RtPPecE3LwE'
 opened_dialogues_db = '/home/julia/workspace/python/BlindDate/opened_dialogues'
 closed_dialogues_db = '/home/julia/workspace/python/BlindDate/closed_dialogues'
 users_db = '/home/julia/workspace/python/BlindDate/users'
+
+UserProperties = namedtuple('UserProperties', ['username', 'revealed'])
