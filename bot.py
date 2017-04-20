3dc55291e938f07c1cfb092db79be330042cd292
Julia Ivanova <ivanova-j-ulia@yandex.ru>
2017-04-20 05:01:30 +0300

    Add working version with limited abilities

--- bot.py
+++ bot.py
@@ -1,41 +1,59 @@
 import config
-from collections import deque
 import shelve
 import telebot
 
 bot = telebot.TeleBot(config.token)
 
-opened_dialogues = shelve.open(config.opened_dialogues_db)
-closed_dialogues = shelve.open(config.closed_dialogues_db)
-waiting_queue = deque()
+# opened_dialogues = shelve.open(config.opened_dialogues_db)
+# closed_dialogues = shelve.open(config.closed_dialogues_db)
+opened_dialogues = {}
+closed_dialogues = {}
+waiting_queue = []
+
+# with shelve.open(config.users_db) as users:
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, 'Finding a partner for you...')
-    if message.chat.id in opened_dialogues:
+    if message.chat.id in opened_dialogues or (len(waiting_queue) != 0 and id == waiting_queue[0]):
         return
-    if waiting_queue.empty() or closed_dialogues[message.chat.id] == waiting_queue.index(0):
+
+    # user = message.from_user.username
+    # if user not in users:
+    #     users[message.chat.id] = (user, False)  # id -> (username, revealed)
+
+    def is_smb_available(id):
+        return len(waiting_queue) != 0 and (id not in closed_dialogues
+                                           or closed_dialogues.get(id) != waiting_queue[0])
+
+    if not is_smb_available(message.chat.id):
         waiting_queue.append(message.chat.id)
     else:
         first = message.chat.id
-        second = waiting_queue.popleft()
+        second = waiting_queue.pop(0)
         opened_dialogues[first] = second
         opened_dialogues[second] = first
-        del closed_dialogues[first]
-        del closed_dialogues[second]
+        if first in closed_dialogues:
+            del closed_dialogues[first]
+        if second in closed_dialogues:
+            del closed_dialogues[second]
         bot.send_message(first, 'Start talking!')
         bot.send_message(second, 'Start talking!')
 
 
 @bot.message_handler(commands=['help'])
 def send_help(message):
-    bot.send_message(message.chat.id, 'I am your mirror... Arghh')
+    bot.send_message(message.chat.id, 'Help')
 
 
-@bot.message_handler(func=lambda m: True)
-def repeat_all_messages(message):  # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, message.text)
+@bot.message_handler(content_types=['text'])
+def reply(message):
+    bot.send_message(opened_dialogues[message.chat.id], message.text)
+
 
 # receiving new messages
-bot.polling(none_stop=True)
+bot.polling()
+
+# opened_dialogues.close()
+# closed_dialogues.close()
--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 token = '343052840:AAHQHcniRnEPMWWL23oLuFC6RtPPecE3LwE'
 opened_dialogues_db = '/home/julia/workspace/python/BlindDate/opened_dialogues'
 closed_dialogues_db = '/home/julia/workspace/python/BlindDate/closed_dialogues'
+users_db = '/home/julia/workspace/python/BlindDate/users'
