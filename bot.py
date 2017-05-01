794c2a0f4270d3d2ce25516327482ff583660d86
Julia Ivanova <ivanova-j-ulia@yandex.ru>
2017-05-02 02:57:17 +0300

    Add logging

--- bot.py
+++ bot.py
@@ -1,6 +1,11 @@
 import config
 import shelve
 import telebot
+import logging
+
+logging.basicConfig(format=u'%(levelname)-8s [%(asctime)s] %(message)s',
+                    level=logging.DEBUG,
+                    filename=u'mylog.log')
 
 bot = telebot.TeleBot(config.token)
 
@@ -16,8 +21,9 @@ with shelve.open(config.users_db) as users:
 
 
     def is_smb_available(user_id):
-        return len(waiting_queue) != 0 and (user_id not in closed_dialogues
-                                            or closed_dialogues.get(user_id) != waiting_queue[0])
+        return len(waiting_queue) != 0 \
+               and (user_id not in closed_dialogues
+                    or closed_dialogues.get(user_id) != waiting_queue[0])
 
 
     def remove_from_list(first, second, list):
@@ -35,17 +41,22 @@ with shelve.open(config.users_db) as users:
 
     @bot.message_handler(commands=['start'])
     def start(message):
+        logging.debug('\start command was received from user {}'
+                      .format(message.chat.id))
         user_id = message.chat.id
         if is_started(user_id):
+            logging.warning('\start command was received for the second time')
             return
 
         bot.send_message(user_id, 'Finding a partner for you...')
+        logging.debug('Started partner finding')
 
         username = message.from_user.username
         add_new_user(user_id, username)
 
         if not is_smb_available(user_id):
             waiting_queue.append(user_id)
+            logging.debug('Add new user to waiting queue')
         else:
             first = user_id
             second = waiting_queue.pop(0)
@@ -53,50 +64,75 @@ with shelve.open(config.users_db) as users:
             remove_from_list(first, second, list=closed_dialogues)
             bot.send_message(first, 'Start talking!')
             bot.send_message(second, 'Start talking!')
+            logging.debug('Conversation started between users: {} and {}'
+                          .format(first, second))
 
 
     @bot.message_handler(commands=['help'])
     def send_help(message):
-        bot.send_message(message.chat.id,
-                         'Send "/start" to start a conversation.\n'
-                         'Send "/leave" to close a conversation.\n'
-                         'Send "/change_room" to change your partner.\n'
-                         'Send "/reveal" to allow bot to show your username to your partner. '
-                         'It would be shown only provided that '
-                         'your partner also sent a reveal command\n')
+        logging.debug('\help command was received from user {}'
+                      .format(message.chat.id))
+        bot.send_message(
+            message.chat.id,
+            'Send "/start" to start a conversation.\n'
+            'Send "/leave" to close a conversation.\n'
+            'Send "/change_room" to change your partner.\n'
+            'Send "/reveal" to allow bot to show your username to your partner. '
+            'It would be shown only provided that '
+            'your partner also sent a reveal command\n')
 
 
     @bot.message_handler(commands=['leave'])
     def leave(message):
+        logging.debug('\leave command was received from user {}'
+                      .format(message.chat.id))
         first = message.chat.id
         if first in waiting_queue:
             waiting_queue.remove(first)
+            logging.debug('Removed user {} from the waiting queue'
+                          .format(first))
         if first not in opened_dialogues:
+            logging.warning('User {} was not in the opened dialogues'
+                            .format(message.chat.id))
             return
         second = opened_dialogues[first]
         remove_from_list(first, second, list=opened_dialogues)
         add_to_list(first, second, list=closed_dialogues)
         bot.send_message(second, 'Your partner left the chat. :(\n'
                                  'Send "/start" to open a new conversation.')
+        logging.debug('Closed dialogue between users: {} and {}'
+                      .format(first, second))
 
 
     @bot.message_handler(commands=['change_room'])
     def change_room(message):
+        logging.debug('\change_room command was received from user {}'
+                      .format(message.chat.id))
         user_id = message.chat.id
         if user_id in waiting_queue:
+            logging.error('User {} can not change room, '
+                          'he is only in the waiting queue'
+                          .format(message.chat.id))
             return
         leave(message)
         start(message)
+        logging.debug('Changed room for user {}'.format(message.chat.id))
 
 
     @bot.message_handler(commands=['reveal'])
     def reveal(message):
+        logging.debug(u'\reveal command was received from user {}'
+                      .format(message.chat.id))
         first = message.chat.id
         if first in waiting_queue:
             bot.send_message(first, 'Your chat has not started yet.')
+            logging.warning('Chat with user {} was not started yet'
+                            .format(first))
             return
         if first not in opened_dialogues:
             bot.send_message(first, 'Start a conversation first.')
+            logging.warning('Chat with user {} was not started yet'
+                            .format(first))
             return
 
         second = opened_dialogues[first]
@@ -104,86 +140,127 @@ with shelve.open(config.users_db) as users:
 
         if first_username is None:
             bot.send_message(first, 'Please, set username first.')
+            logging.warning('Username of user {} was not set yet'
+                            .format(first))
             return
 
         if users[str(second)].revealed:
             second_username = users[str(second)].username
-            bot.send_message(first, 'User @' + second_username + ' revealed his username.')
-            bot.send_message(second, 'User @' + first_username + ' revealed his username.')
+            bot.send_message(
+                first,
+                'User @{} revealed his username.'.format(second_username))
+            bot.send_message(
+                second,
+                'User @{} revealed his username.'.format(first_username))
+            logging.debug('Revealed users {} and {} to each other'
+                          .format(first, second))
         else:
             users[str(first)] = config.UserProperties(first_username, True)
+            logging.debug('Set users {} "revealed"=True'.format(first))
 
 
     @bot.message_handler(content_types=['text'])
     def reply(message):
+        logging.debug('Text message was received from user {}'
+                      .format(message.chat.id))
         user_id = message.chat.id
         if user_id in opened_dialogues:
-            bot.send_message(opened_dialogues[user_id], message.text)
+            partner = opened_dialogues[user_id]
+            bot.send_message(partner,
+                             message.text)
+            logging.debug('Send a text message from {} to {}'
+                          .format(user_id, partner))
 
 
     @bot.message_handler(content_types=['audio'])
     def reply(message):
+        logging.debug('Audio message was received from user {}'
+                      .format(message.chat.id))
         user_id = message.chat.id
         if user_id in opened_dialogues:
-            bot.send_audio(opened_dialogues[user_id], message.audio.file_id)
+            partner = opened_dialogues[user_id]
+            bot.send_audio(partner,
+                           message.audio.file_id)
+            logging.debug('Send an audio message from {} to {}'
+                          .format(user_id, partner))
 
 
     @bot.message_handler(content_types=['sticker'])
     def sticker(message):
+        logging.debug('Sticker was received from user {}'
+                      .format(message.chat.id))
         user_id = message.chat.id
         if user_id in opened_dialogues:
-            bot.send_sticker(opened_dialogues[user_id], message.sticker.file_id)
+            partner = opened_dialogues[user_id]
+            bot.send_sticker(partner,
+                             message.sticker.file_id)
+            logging.debug('Send a sticker from {} to {}'
+                          .format(user_id, partner))
 
 
     @bot.message_handler(content_types=['voice'])
     def sticker(message):
+        logging.debug('Voice was received from user {}'
+                      .format(message.chat.id))
         user_id = message.chat.id
         if user_id in opened_dialogues:
-            bot.send_voice(opened_dialogues[user_id], message.voice.file_id)
+            partner = opened_dialogues[user_id]
+            bot.send_voice(partner,
+                           message.voice.file_id)
+            logging.debug('Send a voice from {} to {}'
+                          .format(user_id, partner))
 
 
     @bot.message_handler(content_types=['document'])
     def sticker(message):
+        logging.debug('Document was received from user {}'
+                      .format(message.chat.id))
         user_id = message.chat.id
         if user_id in opened_dialogues:
-            bot.send_document(opened_dialogues[user_id], message.document.file_id)
-
-
-    @bot.message_handler(content_types=['callback_query'])
-    def sticker(message):
-        user_id = message.chat.id
-        game_short_name = message.callback_query.game_short_name
-        if user_id in opened_dialogues:
-            bot.send_game(opened_dialogues[user_id], game_short_name)
-
-
-    @bot.message_handler(content_types=['game'])  # FIX ME
-    def sticker(message):
-        user_id = message.chat.id
-        game_name = message.game.text
-        print(game_name)
-        #if user_id in opened_dialogues:
-        bot.send_game(user_id, message.game.title)
+            partner = opened_dialogues[user_id]
+            bot.send_document(partner,
+                              message.document.file_id)
+            logging.debug('Send a document from {} to {}'
+                          .format(user_id, partner))
 
 
     @bot.message_handler(content_types=['photo'])
     def sticker(message):
+        logging.debug('Photo was received from user {}'
+                      .format(message.chat.id))
         user_id = message.chat.id
         if user_id in opened_dialogues:
-            bot.send_photo(opened_dialogues[user_id], message.photo[len(message.photo)-1].file_id)
+            partner = opened_dialogues[user_id]
+            bot.send_photo(partner,
+                           message.photo[len(message.photo)-1].file_id)
+            logging.debug('Send a photo from {} to {}'
+                          .format(user_id, partner))
 
 
     @bot.message_handler(content_types=['video'])
     def sticker(message):
+        logging.debug('Video was received from user {}'
+                      .format(message.chat.id))
         user_id = message.chat.id
         if user_id in opened_dialogues:
-            bot.send_video(opened_dialogues[user_id], message.video.file_id)
+            partner = opened_dialogues[user_id]
+            bot.send_video(partner,
+                           message.video.file_id)
+            logging.debug('Send a video from {} to {}'
+                          .format(user_id, partner))
 
 
     @bot.message_handler(content_types=['location'])
     def sticker(message):
+        logging.debug('Location was received from user {}'
+                      .format(message.chat.id))
         user_id = message.chat.id
         if user_id in opened_dialogues:
-            bot.send_location(opened_dialogues[user_id], message.location.latitude, message.location.longitude)
+            partner = opened_dialogues[user_id]
+            bot.send_location(partner,
+                              message.location.latitude,
+                              message.location.longitude)
+            logging.debug('Send a location from {} to {}'
+                          .format(user_id, partner))
 
     bot.polling()
--- config.py
+++ config.py
@@ -4,3 +4,4 @@ token = '343052840:AAHQHcniRnEPMWWL23oLuFC6RtPPecE3LwE'
 users_db = '/home/julia/workspace/python/BlindDate/users.txt'
 
 UserProperties = namedtuple('UserProperties', ['username', 'revealed'])
+
